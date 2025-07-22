ffe1f8648acc7c246b332de1e97c8465fb11547c
Hugh <hugh_gleaves@hotmail.com>
2025-07-22 10:47:06 -0700

    Add a simple unit test.

---
+++ UnitTesting/Test1.cs
@@ -0,0 +1,20 @@
+﻿using Syscode;
+using System.Diagnostics;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var compiler = new SyscodeCompiler(@"..\..\..\..\TestSource\messages.json");
+            var cst = compiler.CompileSourceFile(@"..\..\..\Syscode\builtin.sys");
+            var ast = compiler.GenerateAbstractSyntaxTree(cst);
+            compiler.ProcessDeclarations(ast);
+            compiler.ResolveCompilationReferences(ast);
+            Assert.AreEqual(1015, compiler.Reporter.Messages.First().code);
+        }
+    }
+}
